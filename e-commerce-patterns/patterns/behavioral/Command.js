8722abb02d02d7986c80f7760069633a4932cdb1
phamtuan2005tm-cpu <email_cua_fen@example.com>
2026-05-13 11:43:11 +0700

    Completed part 4

--- e-commerce-patterns/patterns/behavioral/Command.js
+++ e-commerce-patterns/patterns/behavioral/Command.js
@@ -38,12 +38,16 @@ class AddToCartCommand extends Command {
     execute() {
         // TODO: Implement the execute method.
         // It should call the `addProduct` method of the `cartService`.
+        this.cartService.addProduct(this.product);
+        console.log(`Command: Added ${this.product.name} to cart.`);
     }
 
     undo() {
         // TODO: Implement the undo method.
         // It should call the `removeProduct` method of the `cartService`,
         // using the product's ID.
+        this.cartService.removeProduct(this.product.id);
+        console.log(`Command Undo: Removed ${this.product.name} from cart.`);
     }
 }
 
